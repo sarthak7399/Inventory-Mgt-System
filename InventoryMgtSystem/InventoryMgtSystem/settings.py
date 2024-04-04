9f35b0bf37ca4aecffa4bb43767f74b2f274bc0c
Sarthak Sharma <sarthak753@gmail.com>
2024-04-04 21:32:50 +0530

    Homepage

--- InventoryMgtSystem/InventoryMgtSystem/settings.py
+++ InventoryMgtSystem/InventoryMgtSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h4_*7rb^foj9f$grll@u2+*x3=l9jz6p9ywsh(t#^5c*)uawx0'
+SECRET_KEY = 'django-insecure-h4_7rb^foj9f$grll@u2+*x3=l9jz6p9ywsh(t#^5c)uawx0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
